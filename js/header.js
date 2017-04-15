a183f0f7bf0ae8302b9de2662bc27355f27cddb8
kiefersivitz <kmsivitz@gmail.com>
2017-04-15 18:18:34 -0400

    Added footer and updated header.js to reflect changes

--- js/header.js
+++ js/header.js
@@ -1,7 +1,16 @@
 "use strict";
-const headerUrl = "../pages/header.html";
-$.ajax(headerUrl, {
-    success: data => {
-        $("body > *").first().before(data);
-    }
+var headerUrl = "../pages/header.html",
+    footerUrl = "../pages/footer.html";
+
+$(function () {
+    $.ajax(headerUrl, {
+        success: data => {
+            $("body > *").first().before(data);
+        }
+    });
+    $.ajax(footerUrl, {
+        success: data => {
+            $("body > *").last().after(data);
+        }
+    });
 });
